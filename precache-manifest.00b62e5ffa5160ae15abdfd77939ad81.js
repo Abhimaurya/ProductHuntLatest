0d766af78e81f631c8ce01d13d7e3bbfbe50823f
abhimaurya <arm20493@gmail.com>
2020-01-05 21:06:16 +0530

    Updates

---
+++ precache-manifest.00b62e5ffa5160ae15abdfd77939ad81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e83c2fbc0acf055f31b5db00fbbe1133",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5cccf8c0969e09b238a0",
+    "url": "/static/css/2.c51b6406.chunk.css"
+  },
+  {
+    "revision": "7921e1e96c5a2e3c881b",
+    "url": "/static/css/main.15364b9b.chunk.css"
+  },
+  {
+    "revision": "5cccf8c0969e09b238a0",
+    "url": "/static/js/2.e136dab9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.e136dab9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7921e1e96c5a2e3c881b",
+    "url": "/static/js/main.a110ec93.chunk.js"
+  },
+  {
+    "revision": "5594771136811fab885e",
+    "url": "/static/js/runtime-main.29ce243d.js"
+  }
+]);
